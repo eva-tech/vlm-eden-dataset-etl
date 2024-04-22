bbf1a65c12eff337a24e1ccdf352b2aed1cf0cb7
Lenin Luque <xleninx@gmail.com>
2024-04-22 18:09:45 +0200

    feat: PR fixes

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ import os
 
 import psycopg2
 from dotenv import load_dotenv
+from yoyo import get_backend, read_migrations
 
 
 def create_connection_to_source():
@@ -37,3 +38,21 @@ def create_connection_to_destination():
         port=os.getenv("DESTINATION_DATABASE_PORT"),
     )
     return conn
+
+
+def run_general_migrations():
+    """
+    Run database migrations using the yoyo library.
+
+    Reads migrations from the "./general_migrations/" directory and applies them to the destination database.
+    """
+    host_and_database = f"{os.getenv('DESTINATION_DATABASE_HOST')}/{os.getenv('DESTINATION_DATABASE_NAME')}"
+    user_and_password = f"{os.getenv('DESTINATION_DATABASE_USER')}:{os.getenv('DESTINATION_DATABASE_PASS')}"
+    destination_database = f"postgresql://{user_and_password}@{host_and_database}"
+    migrations = read_migrations("./general_migrations/")
+
+    conn_destination = create_connection_to_destination()
+    backend = get_backend(destination_database)
+    backend.apply_migrations(backend.to_apply(migrations))
+
+    conn_destination.close()
